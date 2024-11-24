a2e410eb78ad65e8dbb4162b52b9e6eeebbe06b6
SanskarOmer <sanskaromer7@gmail.com>
2024-11-24 23:46:57 +0530

    created a basic server

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const catMe = require("cat-me");
+const http = require('http');
 
+const server = http.createServer((req, res) =>{
+    res.end('Hello, World!\n');
+});
 
-console.log(catMe())
+server.listen(8080, () => {
+    console.log('Server is running on port 8080');
+});
