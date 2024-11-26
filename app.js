5ff0f1e196728af3574532b50c0d2895e246d1ce
SanskarOmer <sanskaromer7@gmail.com>
2024-11-26 21:25:37 +0530

    added view engine and created a middleware

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use((req, res, next)=>{
+    console.log(`New request received at ${new Date().toISOString()}`);
+     next();
+});
+
+app.get('/', (req, res) => { 
+    res.render('index');
+});
+
+app.listen(3000);
