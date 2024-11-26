1179aaa892c9efef00fc1baf1b17837e5be337b2
SanskarOmer <sanskaromer7@gmail.com>
2024-11-26 20:41:21 +0530

    loaded express module

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-const http = require('http');
-
-const server = http.createServer((req, res) =>{
-    if (req.url=='/'){
-        res.end('this is a home page');
-    }
-    else if (req.url=='/about'){
-        res.end('this is the about page');
-    }
-    else {
-        res.writeHead(404);
-        res.end('Page not found');
-    }
-});
-
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+const express = require('express');
